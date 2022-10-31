088230017ccd151b0e955764d2d79c89a406078e
Leonardo Laguna Ruiz <modlfo@gmail.com>
2022-10-31 16:24:12 +0200

    Adds option to set the indentation size.

--- lib/pla.ml
+++ lib/pla.ml
@@ -17,6 +17,8 @@
    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *)
 
+type options = { indent_size : int }
+
 type buffer =
   { append : string -> unit
   ; content : unit -> string
@@ -24,11 +26,14 @@ type buffer =
   ; mutable indent : int
   ; mutable space : string
   ; mutable indented : bool
+  ; options : options
   }
 
 type t = buffer -> unit
 
-let buffer_new () =
+let default_options = { indent_size = 3 }
+
+let buffer_new options =
   let b = Buffer.create 128 in
   { append = (fun s -> Buffer.add_string b s)
   ; content = (fun () -> Buffer.contents b)
@@ -36,10 +41,11 @@ let buffer_new () =
   ; indent = 0
   ; space = ""
   ; indented = false
+  ; options
   }
+;;
 
-
-let buffer_new_file (file : string) =
+let buffer_new_file options (file : string) =
   let c = open_out file in
   { append = (fun s -> output_string c s)
   ; content = (fun () -> "")
@@ -47,59 +53,52 @@ let buffer_new_file (file : string) =
   ; indent = 0
   ; space = ""
   ; indented = false
+  ; options
   }
-
+;;
 
 let buffer_contents t : string = t.content ()
-
 let buffer_close t : unit = t.close ()
 
 let buffer_newline t =
-  t.append "\n" ;
+  t.append "\n";
   t.indented <- false
   [@@inline always]
-
+;;
 
 let buffer_indent t : unit =
-  t.indent <- t.indent + 1 ;
-  t.space <- String.make (t.indent * 3) ' ' ;
+  t.indent <- t.indent + 1;
+  t.space <- String.make (t.indent * t.options.indent_size) ' ';
   buffer_newline t
   [@@inline always]
-
+;;
 
 let buffer_outdent t : unit =
-  t.indent <- t.indent - 1 ;
-  if t.indent < 0 then
-    failwith "Cannot outdent more" ;
-  t.space <- String.make (t.indent * 3) ' '
+  t.indent <- t.indent - 1;
+  if t.indent < 0 then failwith "Cannot outdent more";
+  t.space <- String.make (t.indent * t.options.indent_size) ' '
   [@@inline always]
-
+;;
 
 let buffer_append t (s : string) : unit =
-  if not t.indented then begin
-    t.append t.space ;
-    t.indented <- true
-  end ;
+  if not t.indented
+  then (
+    t.append t.space;
+    t.indented <- true);
   t.append s
   [@@inline always]
-
+;;
 
 let buffer_apply (t : t) (buffer : buffer) : unit = t buffer [@@inline always]
-
 let make (f : buffer -> unit) : t = f [@@inline always]
 
 (* Builtin templates *)
 
 let unit : t = fun _ -> ()
-
 let newline : t = fun buffer -> buffer_newline buffer
-
 let comma : t = fun buffer -> buffer_append buffer ","
-
 let commaspace : t = fun buffer -> buffer_append buffer ", "
-
 let semi : t = fun buffer -> buffer_append buffer ";"
-
 let space : t = fun buffer -> buffer_append buffer " "
 
 (* Templates of basic types *)
@@ -108,64 +107,63 @@ let string (s : string) : t = fun buffer -> buffer_append buffer s
 
 let string_quoted (s : string) : t =
  fun buffer ->
-  buffer_append buffer "\"" ;
-  buffer_append buffer s ;
+  buffer_append buffer "\"";
+  buffer_append buffer s;
   buffer_append buffer "\""
-
+;;
 
 let int (i : int) : t = fun buffer -> buffer_append buffer (string_of_int i)
 
 let float (f : float) : t =
  fun buffer ->
   let s = string_of_float f in
-  buffer_append buffer s ;
-  if s.[String.length s - 1] = '.' then
-    buffer_append buffer "0"
-
+  buffer_append buffer s;
+  if s.[String.length s - 1] = '.' then buffer_append buffer "0"
+;;
 
 let bool (b : bool) : t =
  fun buffer ->
   let s = if b then "true" else "false" in
   buffer_append buffer s
-
+;;
 
 (* Functions to wrap templates *)
 
 let quote (t : t) : t =
  fun buffer ->
-  buffer_append buffer "\"" ;
-  t buffer ;
+  buffer_append buffer "\"";
+  t buffer;
   buffer_append buffer "\""
-
+;;
 
 let parenthesize (t : t) : t =
  fun buffer ->
-  buffer_append buffer "(" ;
-  t buffer ;
+  buffer_append buffer "(";
+  t buffer;
   buffer_append buffer ")"
-
+;;
 
 let indent (t : t) : t =
  fun buffer ->
-  buffer_indent buffer ;
-  t buffer ;
+  buffer_indent buffer;
+  t buffer;
   buffer_outdent buffer
-
+;;
 
 let wrap (l : t) (r : t) (t : t) : t =
  fun buffer ->
-  l buffer ;
-  t buffer ;
+  l buffer;
+  t buffer;
   r buffer
-
+;;
 
 (* Functions to append templates *)
 
 let append (t1 : t) (t2 : t) : t =
  fun buffer ->
-  t1 buffer ;
+  t1 buffer;
   t2 buffer
-
+;;
 
 let join (elems : t list) : t = fun buffer -> List.iter (fun a -> a buffer) elems
 
@@ -175,21 +173,21 @@ let join_sep (sep : t) (elems : 'a list) : t =
     | [] -> ()
     | [ h ] -> h buffer
     | h :: t ->
-        h buffer ;
-        sep buffer ;
-        loop t
+      h buffer;
+      sep buffer;
+      loop t
   in
   loop elems
-
+;;
 
 let join_sep_all (sep : t) (elems : 'a list) : t =
  fun buffer ->
   List.iter
     (fun h ->
-      h buffer ;
-      sep buffer )
+      h buffer;
+      sep buffer)
     elems
-
+;;
 
 let map_join (f : 'a -> t) (elems : 'a list) : t = fun buffer -> List.iter (fun a -> f a buffer) elems
 
@@ -199,35 +197,35 @@ let map_sep (sep : t) (f : 'a -> t) (elems : 'a list) : t =
     | [] -> ()
     | [ h ] -> (f h) buffer
     | h :: t ->
-        (f h) buffer ;
-        sep buffer ;
-        loop t
+      (f h) buffer;
+      sep buffer;
+      loop t
   in
   loop elems
-
+;;
 
 let map_sep_all (sep : t) (f : 'a -> t) (elems : 'a list) : t =
  fun buffer ->
   List.iter
     (fun h ->
-      (f h) buffer ;
-      sep buffer )
+      (f h) buffer;
+      sep buffer)
     elems
-
+;;
 
 let ( ++ ) (t1 : t) (t2 : t) : t = append t1 t2
 
-let print (t : t) : string =
-  let buffer = buffer_new () in
-  t buffer ;
+let print ?(options = default_options) (t : t) : string =
+  let buffer = buffer_new options in
+  t buffer;
   buffer_contents buffer
+;;
 
-
-let write (file : string) (t : t) : unit =
-  let buffer = buffer_new_file file in
-  t buffer ;
+let write ?(options = default_options) (file : string) (t : t) : unit =
+  let buffer = buffer_new_file options file in
+  t buffer;
   buffer_close buffer
-
+;;
 
 type compiled =
   { tokens : Pla_tokens.s list
@@ -245,63 +243,59 @@ let collectSlots tokens =
     | [] -> acc
   in
   loop StringMap.empty tokens
-
+;;
 
 let create text =
   let tokens = Pla_lex.tokenize text in
   let slots = StringMap.fold (fun key value acc -> (key, value) :: acc) (collectSlots tokens) [] in
   { tokens; slots; filled = [] }
-
+;;
 
 let set_generic name value type_ compiled =
   let rec loop acc slots =
     match slots with
     | (var, found_type_) :: t when var = name ->
-        if type_ = found_type_ then
-          (var, value), acc @ t
-        else
-          failwith ("The type for template does not match: " ^ name)
+      if type_ = found_type_ then (var, value), acc @ t else failwith ("The type for template does not match: " ^ name)
     | h :: t -> loop (h :: acc) t
     | [] ->
-        let available = String.concat ", " (List.map (fun (s, _) -> s) compiled.slots) in
-        failwith ("The slot '" ^ name ^ "' could not be filled. " ^ available)
+      let available = String.concat ", " (List.map (fun (s, _) -> s) compiled.slots) in
+      failwith ("The slot '" ^ name ^ "' could not be filled. " ^ available)
   in
   let filled1, slots = loop [] compiled.slots in
   { compiled with filled = filled1 :: compiled.filled; slots }
-
+;;
 
 let set name value compiled = set_generic name value Pla_tokens.Template compiled
-
 let seti name value compiled = set_generic name (int value) Pla_tokens.Int compiled
-
 let setf name value compiled = set_generic name (float value) Pla_tokens.Float compiled
-
 let sets name value compiled = set_generic name (string value) Pla_tokens.String compiled
 
 let close compiled buffer =
-  if compiled.slots <> [] then
+  if compiled.slots <> []
+  then (
     let available = String.concat ", " (List.map (fun (s, _) -> s) compiled.slots) in
-    failwith ("The template is not filled. " ^ available)
+    failwith ("The template is not filled. " ^ available))
   else
     let open Pla_tokens in
     let rec loop tokens =
       match tokens with
       | N :: t ->
-          buffer_newline buffer ;
-          loop t
+        buffer_newline buffer;
+        loop t
       | I :: t ->
-          buffer_indent buffer ;
-          loop t
+        buffer_indent buffer;
+        loop t
       | O :: t ->
-          buffer_outdent buffer ;
-          loop t
+        buffer_outdent buffer;
+        loop t
       | T s :: t ->
-          buffer_append buffer s ;
-          loop t
+        buffer_append buffer s;
+        loop t
       | V (name, _, _) :: t ->
-          let temp = List.assoc name compiled.filled in
-          temp buffer ;
-          loop t
+        let temp = List.assoc name compiled.filled in
+        temp buffer;
+        loop t
       | [] -> ()
     in
     loop compiled.tokens
+;;
--- ppx/pla_ppx.ml
+++ ppx/pla_ppx.ml
@@ -25,27 +25,16 @@ module PlaMapper = struct
   open Ast_helper
 
   let buffer_id = "__buffer__"
-
   let makeLident (str : string) : Longident.t Location.loc = Longident.parse str |> Ocaml_common.Location.mknoloc
-
   let buffer = Exp.ident (makeLident buffer_id)
-
   let newline = Exp.ident (makeLident "Pla.buffer_newline")
-
   let indent = Exp.ident (makeLident "Pla.buffer_indent")
-
   let outdent = Exp.ident (makeLident "Pla.buffer_outdent")
-
   let append = Exp.ident (makeLident "Pla.buffer_append")
-
   let papply = Exp.ident (makeLident "Pla.buffer_apply")
-
   let pint = Exp.ident (makeLident "Pla.int")
-
   let pfloat = Exp.ident (makeLident "Pla.float")
-
   let pstring = Exp.ident (makeLident "Pla.string")
-
   let unit = Exp.construct (makeLident "()") None
 
   let offsetPosition (displacement : int) (pos1 : Lexing.position) (pos2 : Lexing.position) : Lexing.position =
@@ -55,7 +44,7 @@ module PlaMapper = struct
       ; pos_bol = pos1.pos_bol + pos2.pos_bol
       ; pos_cnum = pos1.pos_cnum + pos2.pos_cnum + displacement
       }
-
+  ;;
 
   let offsetLocation (displacement : int) (loc1 : Location.t) (loc2 : Location.t) : Location.t =
     Location.
@@ -63,7 +52,7 @@ module PlaMapper = struct
         loc_start = offsetPosition displacement loc1.loc_start loc2.loc_start
       ; loc_end = offsetPosition displacement loc1.loc_start loc2.loc_end
       }
-
+  ;;
 
   let mkVar (var_loc : Location.t) (v : string) = Ocaml_common.Location.mkloc (Longident.parse v) var_loc
 
@@ -73,7 +62,7 @@ module PlaMapper = struct
     | Pla_tokens.Float -> Typ.constr (makeLident "float") []
     | Pla_tokens.String -> Typ.constr (makeLident "string") []
     | Pla_tokens.Template -> Typ.constr (makeLident "Pla.t") []
-
+  ;;
 
   let constString s = Const.string s
 
@@ -84,48 +73,47 @@ module PlaMapper = struct
     | Pla_tokens.O -> Exp.apply ~loc outdent [ Nolabel, buffer ]
     | Pla_tokens.T txt -> Exp.apply ~loc append [ Nolabel, buffer; Nolabel, Exp.constant (constString txt) ]
     | Pla_tokens.V (v, vartype, loc_ref) ->
-        let var_loc = offsetLocation displacement loc loc_ref in
-        let v_exp = Exp.constraint_ ~loc:var_loc (Exp.ident ~loc:var_loc (mkVar var_loc v)) (template_type vartype) in
-        ( match vartype with
-        | Pla_tokens.Template -> Exp.apply ~loc papply [ Nolabel, v_exp; Nolabel, buffer ]
-        | Pla_tokens.Int -> Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pint [ Nolabel, v_exp ]; Nolabel, buffer ]
-        | Pla_tokens.Float ->
-            Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pfloat [ Nolabel, v_exp ]; Nolabel, buffer ]
-        | Pla_tokens.String ->
-            Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pstring [ Nolabel, v_exp ]; Nolabel, buffer ] )
-
+      let var_loc = offsetLocation displacement loc loc_ref in
+      let v_exp = Exp.constraint_ ~loc:var_loc (Exp.ident ~loc:var_loc (mkVar var_loc v)) (template_type vartype) in
+      (match vartype with
+      | Pla_tokens.Template -> Exp.apply ~loc papply [ Nolabel, v_exp; Nolabel, buffer ]
+      | Pla_tokens.Int -> Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pint [ Nolabel, v_exp ]; Nolabel, buffer ]
+      | Pla_tokens.Float -> Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pfloat [ Nolabel, v_exp ]; Nolabel, buffer ]
+      | Pla_tokens.String ->
+        Exp.apply ~loc papply [ Nolabel, Exp.apply ~loc pstring [ Nolabel, v_exp ]; Nolabel, buffer ])
+  ;;
 
   let makeExpSeq (loc : Location.t) (displacement : int) (sl : Pla_tokens.s list) =
     List.fold_right (fun a s -> Exp.sequence (makeExp loc displacement a) s) sl unit
-
+  ;;
 
   let makeTemplateExp (loc : Location.t) (displacement : int) (sl : Pla_tokens.s list) : expression =
     let pat = Pat.var (Ocaml_common.Location.mknoloc buffer_id) in
     let fun_ = Exp.fun_ ~loc Nolabel None pat (makeExpSeq loc displacement sl) in
     Exp.apply ~loc (Exp.ident (makeLident "Pla.make")) [ Nolabel, fun_ ]
-
+  ;;
 
   let mapper ~loc ~path (expr : expression) : expression =
     let _ = loc in
     let _ = path in
     match expr with
     | { pexp_desc = Pexp_constant (Pconst_string (text, loc, Some "pla")); _ } ->
-        let tokens = Pla_lex.tokenize text in
-        let displacement = 5 in
-        let pla_exp = makeTemplateExp loc displacement tokens in
-        pla_exp
+      let tokens = Pla_lex.tokenize text in
+      let displacement = 5 in
+      let pla_exp = makeTemplateExp loc displacement tokens in
+      pla_exp
     | { pexp_desc = Pexp_constant (Pconst_string (text, loc, _)); _ } ->
-        let tokens = Pla_lex.tokenize text in
-        let displacement = 2 in
-        let pla_exp = makeTemplateExp loc displacement tokens in
-        pla_exp
+      let tokens = Pla_lex.tokenize text in
+      let displacement = 2 in
+      let pla_exp = makeTemplateExp loc displacement tokens in
+      pla_exp
     | _ -> expr
+  ;;
 end
 
 let pla_ext =
   Extension.declare "pla" Ppxlib.Extension.Context.Expression Ast_pattern.(single_expr_payload __) PlaMapper.mapper
-
+;;
 
 let pla_rule = Ppxlib.Context_free.Rule.extension pla_ext
-
 let () = Driver.register_transformation ~rules:[ pla_rule ] "pla"
